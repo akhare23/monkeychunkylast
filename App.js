aae7b787a6ce39cf2b219ada70e2c04c58ee4642
Aayan <aayankhare23@gmail.com>
2021-04-26 16:56:18 -0700

    mclast

--- App.js
+++ App.js
@@ -1,67 +1,96 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
-import {Header} from "react-native-elements"
-import db from "./localdb"
-import PhonesSoundButton from "./components/PhonesSoundButton"
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+} from "react-native";
+import { Header } from "react-native-elements";
+import db from "./localdb";
+import PhonesSoundButton from "./components/PhonesSoundButton";
 
 export default class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      text:"",
-      displaytext:"",
-      chunks:[],
-      phones:[]
-    }
+      text: "",
+      displaytext: "",
+      chunks: [],
+      phones: [],
+    };
   }
-  render(){
-   
-   console.log(this.state.phones);
-  return (
-    <View>
-    <Header backgroundColor = {"green"} centerComponent = {{text:"Monkey Chunky App", style:{fontSize:15, fontWeight:"bold"}}}>
-    
-    </Header>
-    <Image source = {{uri:"https://www.shareicon.net/data/128x128/2015/08/06/80805_face_512x512.png"}} style = {{width:100,height:100, alignSelf:"center"}}></Image>
-    <TextInput style = {styles.inputBox} onChangeText = {(text)=>{
-      this.setState({text:text})
-    }}></TextInput>
-    <TouchableOpacity style = {{backgroundColor:"orange", margin:20, padding:20, alignItems:"center", borderRadius:20}} onPress = {()=>{
-      this.setState({chunks:db[this.state.text].chunks, phones:db[this.state.text]["phones"]})
-    }}>
-      <Text>Submit</Text>
-    </TouchableOpacity>
-    
-    <View>
-    {
-     
-  this.state.chunks.map((i, index)=>(
-    <PhonesSoundButton phonesSound = {this.state.phones[index]} chunkText = {this.state.chunks[index]}/>
+  render() {
+    console.log(this.state.phones);
+    console.log(db[this.state.text]);
+    return (
+      <View>
+        <Header
+          backgroundColor={"green"}
+          centerComponent={{
+            text: "Monkey Chunky App",
+            style: { fontSize: 15, fontWeight: "bold" },
+          }}
+        ></Header>
+        <Image
+          source={{
+            uri:
+              "https://www.shareicon.net/data/128x128/2015/08/06/80805_face_512x512.png",
+          }}
+          style={{ width: 100, height: 100, alignSelf: "center" }}
+        ></Image>
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={(text) => {
+            this.setState({ text: text });
+          }}
+        ></TextInput>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "orange",
+            margin: 20,
+            padding: 20,
+            alignItems: "center",
+            borderRadius: 20,
+          }}
+          onPress={() => {
+            this.setState({
+              chunks: db[this.state.text.toLowerCase().trim()].chunks,
+              phones: db[this.state.text.toLowerCase().trim()]["phones"],
+            });
+          }}
+        >
+          <Text>Submit</Text>
+        </TouchableOpacity>
 
- 
-  
-  ))
-  
-  }
-  </View>
-
-     </View>
-     
-  );
+        <View>
+          {
+            //condition ? isTrue: isFalse
+            db[this.state.text.toLowerCase().trim()] === undefined
+              ? Alert.alert("Word not Found")
+              : this.state.chunks.map((i, index) => (
+                  <PhonesSoundButton
+                    phonesSound={this.state.phones[index]}
+                    chunkText={this.state.chunks[index]}
+                    buttonIndex={index}
+                  />
+                ))
+          }
+        </View>
+      </View>
+    );
   }
 }
 const styles = StyleSheet.create({
-  inputBox:{
-    borderColor:"blue",
-    borderWidth:2,
-    marginTop:20,
-    padding:20,
-    textAlign:"center",
-    width:"60%",
-    alignSelf:"center"
-  }
-})
-  
-
-
-
+  inputBox: {
+    borderColor: "blue",
+    borderWidth: 2,
+    marginTop: 20,
+    padding: 20,
+    textAlign: "center",
+    width: "60%",
+    alignSelf: "center",
+  },
+});
--- components/PhonesSoundButton.js
+++ components/PhonesSoundButton.js
@@ -1,37 +1,64 @@
-import * as React from "react"
-import {TouchableOpacity, Text, StyleSheet} from "react-native"
-import {Audio} from "expo-av"
+import * as React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { Audio } from "expo-av";
 
-
-export default class PhonesSoundButton extends React.Component{
-    playSound  = async ()=>{
-        await Audio.Sound.createAsync({
-            uri:"https://s3-whitehatjrcontent.whjr.online/phones/"+this.props.phonesSound+".mp3"
-        },
-        {shouldPlay:true}
-        )
-      }
-    render(){
-        return(
-            <TouchableOpacity style = {styles.buttonStyles} onPress = {this.playSound}>
-                <Text style = {styles.textStyles}>{this.props.chunkText}</Text>
-            </TouchableOpacity>
-        )
-    }
+export default class PhonesSoundButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hasBeenPressed: false,
+    };
+  }
+  playSound = async () => {
+    await Audio.Sound.createAsync(
+      {
+        uri:
+          "https://s3-whitehatjrcontent.whjr.online/phones/" +
+          this.props.phonesSound +
+          ".mp3",
+      },
+      { shouldPlay: true }
+    );
+  };
+  render() {
+    return (
+      <TouchableOpacity
+        style={
+          this.state.hasBeenPressed === this.props.buttonIndex
+            ? [styles.buttonStyles, { backgroundColor: "green" }]
+            : styles.buttonStyles
+        }
+        onPress={() => {
+          this.playSound();
+          this.setState({ hasBeenPressed: this.props.buttonIndex });
+        }}
+      >
+        <Text
+          style={
+            this.state.hasBeenPressed === this.props.buttonIndex
+              ? [styles.textStyles, { fontStyle: "italic" }]
+              : styles.textStyles
+          }
+        >
+          {this.props.chunkText}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
 }
 const styles = StyleSheet.create({
-    buttonStyles:{
-      backgroundColor:"gray",
-      alignItems:"center",
-      borderRadius:30,
-      margin:20,
-      padding:10,
-      borderWidth:0.4,
-      alignSelf:"center",
-      width:"10%"
-    },
-    textStyles:{
-        fontWeight:"bold",
-        fontSize:20
-    }
-  })
+  buttonStyles: {
+    backgroundColor: "gray",
+    alignItems: "center",
+    borderRadius: 30,
+    margin: 20,
+    padding: 10,
+    borderWidth: 0.4,
+    alignSelf: "center",
+    width: "10%",
+  },
+  textStyles: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
